46a4122d7b6601cbc0f48d486205ba6d4c6cf421
saynasadeghipour <saynasadeghipour@gmail.com>
2024-05-17 21:55:56 +0330

    exercise 6

---
+++ Ex6/Ex6/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace sayna6
+{
+    class Program
+    {
+        static Semaphore s = new Semaphore(2, 2);
+        static Mutex mut = new Mutex(false);
+        static readonly object lockObject = new object();
+        private static AutoResetEvent event_1 = new AutoResetEvent(true);
+
+        static readonly object lockMontiror = new object();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                //Thread th = new Thread( waitOne);
+                //Thread th = new Thread(lockFunc);
+                //Thread th = new Thread(autoFunc);
+                Thread th = new Thread(mutexFunc);
+                Thread.monitor(th);
+                th.Start();
+            }
+
+
+            Console.ReadKey();
+        }
+        private static void waitOne(object obj)
+        {
+            s.WaitOne();
+            Console.WriteLine("start with: " + obj.ToString());
+            Thread.Sleep(1000);
+            Console.WriteLine("end with: " + obj.ToString());
+            s.Release();
+        }
+        private static void lockFunc(object obj)
+        {
+
+            lock (lockMontiror)
+            {
+                Console.WriteLine("start with: " + obj.ToString());
+                Thread.Sleep(1000);
+                Console.WriteLine("end with: " + obj.ToString());
+                Monitor.Pulse(lockMontiror);
+                Monitor.Wait(lockMontiror);
+            }
+
+        }
+        private static void mutexFunc(object obj)
+        {
+            mut.WaitOne();
+            Console.WriteLine("start with: " + obj.ToString());
+            Thread.Sleep(1000);
+            Console.WriteLine("end with: " + obj.ToString());
+            mut.ReleaseMutex();
+        }
+        private static void monitorFunc(object obj)
+        {
+            Monitor.Enter(lockObject);
+
+            Console.WriteLine("start with: " + obj.ToString());
+            Thread.Sleep(1000);
+            Console.WriteLine("end with: " + obj.ToString());
+
+            Monitor.Exit(lockObject);
+
+        }
+        private static void autoFunc(object obj)
+        {
+            Console.WriteLine("start with: " + obj.ToString());
+            Thread.Sleep(1000);
+            Console.WriteLine("end with: " + obj.ToString());
+            event_1.WaitOne();
+        }
+    }
+}
